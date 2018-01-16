65aa2bdd90499399effc95c8e98af1bb47a83e76
Gennaro Del Sorbo <JD78@JD-macbook.local>
2018-01-17 00:58:02 +0100

    Unit tests

---
+++ partitioner_test.go
@@ -0,0 +1,91 @@
+package partitioner
+
+import (
+	"testing"
+	"time"
+)
+
+type partition struct {
+	partition int64
+}
+
+func (p partition) GetPartition() int64 {
+	return p.partition
+}
+
+func Test_partitioner_HandleInSequence(t *testing.T) {
+	p := Partitioner(30, 5000)
+
+	firstExecuted := false
+	secondExecuted := false
+
+	f1 := func(done chan bool) {
+		firstExecuted = true
+		time.Sleep(2 * time.Second)
+		done <- true
+	}
+
+	f2 := func(done chan bool) {
+		secondExecuted = true
+		time.Sleep(2 * time.Second)
+		done <- true
+	}
+
+	p.HandleInSequence(f1, partition{1})
+	p.HandleInSequence(f2, partition{1})
+	time.Sleep(20 * time.Millisecond)
+
+	if !firstExecuted || secondExecuted {
+		t.Error("Functions executed not in the right sequence")
+	}
+}
+
+func Test_partitioner_HandleConcurrently(t *testing.T) {
+	p := Partitioner(30, 5000)
+
+	firstExecuted := false
+	secondExecuted := false
+
+	f1 := func(done chan bool) {
+		firstExecuted = true
+		time.Sleep(2 * time.Second)
+		done <- true
+	}
+
+	f2 := func(done chan bool) {
+		secondExecuted = true
+		time.Sleep(2 * time.Second)
+		done <- true
+	}
+
+	p.HandleInSequence(f1, partition{1})
+	p.HandleInSequence(f2, partition{2})
+	time.Sleep(20 * time.Millisecond)
+
+	if !firstExecuted || !secondExecuted {
+		t.Error("Both functions not executed")
+	}
+}
+
+func Test_partitioner_HandleInfiniteRetries(t *testing.T) {
+	p := Partitioner(30, 5000)
+
+	secondExecuted := false
+
+	f1 := func(done chan bool) {
+		done <- false
+	}
+
+	f2 := func(done chan bool) {
+		secondExecuted = true
+		done <- true
+	}
+
+	p.HandleInSequence(f1, partition{1})
+	p.HandleInSequence(f2, partition{1})
+	time.Sleep(20 * time.Millisecond)
+
+	if secondExecuted {
+		t.Error("Second function should not be executed")
+	}
+}
