6645fc87ff31b350910c17aa1328290e2109a7d8
Gennaro Del Sorbo <JD78@JD-macbook.local>
2018-01-17 00:02:14 +0100

    implementation completed

--- partitioner.go
+++ partitioner.go
@@ -1,19 +1,27 @@
 package partitioner
 
 import (
-	"sync/atomic"
+	"time"
 )
 
-var roundRobin int32 = 0
-
-type Handler func()
+// Handler high order function to be executed
+// done chan bool: confirmation channel, true for completed, false for error and infinite retry
+type Handler func(done chan bool)
 
 type partitioner struct {
 	nPart      int
 	partitions []chan Handler
 }
 
-func Partitioner(partitions int) partitioner {
+// PartitionId interface to be passed in HandleInSequence
+type PartitionId interface {
+	GetPartition() int64
+}
+
+// Partitioner create a new partition object
+// partitions: number of partitions
+// maxWaitingRetryMilliseconds: max waiting time between retries
+func Partitioner(partitions, maxWaitingRetryMilliseconds int) partitioner {
 	p := make([]chan Handler, partitions, partitions)
 	for i := 0; i < len(p); i++ {
 		p[i] = make(chan Handler, 1000)
@@ -23,7 +31,21 @@ func Partitioner(partitions int) partitioner {
 		go func(partId int) {
 			for {
 				f := <-p[partId]
-				f()
+				done := make(chan bool, 1)
+				result := false
+				waiting := 20
+				for !result {
+					f(done)
+					result = <-done
+					if !result {
+						time.Sleep(time.Duration(waiting) * time.Millisecond)
+						if waiting < maxWaitingRetryMilliseconds {
+							waiting = waiting * 2
+						} else {
+							waiting = maxWaitingRetryMilliseconds
+						}
+					}
+				}
 			}
 		}(i)
 	}
@@ -31,14 +53,10 @@ func Partitioner(partitions int) partitioner {
 	return partitioner{partitions, p}
 }
 
-func (p partitioner) HandleInSequence(handler Handler, partitionId int64) {
-	partition := partitionId % int64(p.nPart)
-
-	p.partitions[partition] <- handler
-}
-
-func (p partitioner) HandleRoundRobin(handler Handler) {
-	i := atomic.AddInt32(&roundRobin, 1)
-	partition := int(i) % p.nPart
+// HandleInSequence handles the handler high order function in sequence based on the resolved partitionId
+// handler: high order function to execute
+// partitionId: PartitionId interface to get an int64 partition
+func (p partitioner) HandleInSequence(handler Handler, partitionId PartitionId) {
+	partition := partitionId.GetPartition() % int64(p.nPart)
 	p.partitions[partition] <- handler
 }
